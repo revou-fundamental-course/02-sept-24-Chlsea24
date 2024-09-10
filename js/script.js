7f824a3f696839f5036a5495f3718b9fb12dd5fb
Chelsea Effendi <s32220030@student.ubm.ac.id>
2024-09-10 14:53:54 +0700

    Triangle all done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-function hitung_luas(){
+function hitung_keliling_segitiga(){
+    let sisi1 = document.getElementById("panjang1").value;
+    let sisi2 = document.getElementById("panjang2").value;
+    let sisi3 = document.getElementById("panjang3").value;
+
+    if (sisi1 === "" || sisi2 === "" || sisi3 === "") {
+        document.getElementById('hasil_keliling').textContent = "[Kamu belum input semua angkanya!]";
+    } else {
+        let hasil =parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3);
+        document.getElementById('hasil_keliling').textContent = `[Hasilnya adalah ${hasil}]`;
+    }
+}
+
+function clear_keliling_segitiga(){
+    document.getElementById('panjang1').value = '';
+    document.getElementById('panjang2').value = '';
+    document.getElementById('panjang3').value = '';
+    document.getElementById('hasil_keliling').textContent = ''; 
+}
+
+function hitung_luas_segitiga(){
     let alas = document.getElementById("panjang_alas").value;
     let tinggi = document.getElementById("tinggi").value;
     
-    if (alas === "" && tinggi === "") {
-        document.getElementById('hasil_luas').textContent = "[Kamu belum input angka sama sekali!]";
+    if (alas === "" || tinggi === "") {
+        document.getElementById('hasil_luas').textContent = "[Kamu belum input semua angkanya!]";
     } else {
         let hasil = 0.5 * parseInt(alas) * parseInt(tinggi);
         document.getElementById('hasil_luas').textContent = `[Hasilnya adalah ${hasil}]`;
@@ -11,9 +31,7 @@ function hitung_luas(){
 }
 
 function clear_luas_segitiga(){
-    // Clear the selected-alasan & selected-pendonor input field
     document.getElementById('panjang_alas').value = '';
     document.getElementById('tinggi').value = '';
-    document.getElementById('hasil_luas').textContent = '';
-    
+    document.getElementById('hasil_luas').textContent = ''; 
 }
